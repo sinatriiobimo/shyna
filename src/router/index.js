567bd66e9b0fdfd0d94c940ef1088ff8b610b63b
Synamount <44100152+Synamount@users.noreply.github.com>
2020-04-04 19:50:39 +0700

    upload vol 1

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import ShoopingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/cart',
+    name: 'ShoppingCart',
+    component: ShoopingCart
+  },
+  {
+    path: '/success',
+    name: 'Success',
+    component: Success
+  }
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShyna></HeaderShyna>
+    <HeroShyna></HeroShyna>
+    <WomenShyna></WomenShyna>
+    <InstaShyna></InstaShyna>
+    <PartnerShyna></PartnerShyna>
+    <FooterShyna></FooterShyna>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShyna from "../components/HeaderShyna";
+import HeroShyna from "../components/HeroShyna";
+import WomenShyna from "../components/WomenShyna";
+import InstaShyna from "../components/InstaShyna";
+import PartnerShyna from "../components/PartnerShyna";
+import FooterShyna from "../components/FooterShyna";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShyna,
+    HeroShyna,
+    WomenShyna,
+    InstaShyna,
+    PartnerShyna,
+    FooterShyna
+  }
+};
+</script>
